87f4d291452818b8fc1de1f31afe78c394ff2600
hunny2000 <abhinavbhasin58@gmail.com>
2021-09-08 05:24:21 +0530

    Final

---
+++ VisualEZ-Assessment/Assets/Scripts/TextureSelector.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System.IO;
+using UnityEngine.UI;
+
+public class TextureSelector : MonoBehaviour
+{
+    public GameObject UI_ShowCase;
+    public Material TempMat;
+    public GameObject EditWall;
+    public List<Texture2D> texture;
+    [SerializeField] Shader CheckerShader;
+
+    string TextureURL;
+    List<JSONNode> DATA;
+
+    string dir;
+
+    void Awake()
+    {
+        dir = Path.Combine(Application.dataPath,  "Resources");
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        texture = new List<Texture2D>();
+        DATA = new List<JSONNode>();
+        TextureURL = "https://quicklook.orientbell.com/Task/gettiles.php";
+
+        StartCoroutine(GetTileData());
+        LoadTextures();
+    }
+
+    public void CreateMaterial(string MatName)
+    {
+        TempMat = new Material(CheckerShader);
+        TempMat.name = MatName;
+        TempMat.SetTexture("Texture_0", Texture2D.grayTexture);
+        TempMat.SetTexture("Texture_1", Texture2D.whiteTexture);
+        UI_ShowCase.GetComponent<MeshRenderer>().material = TempMat;
+    }
+
+    public void LoadTextures()
+    {
+        //Load
+        DirectoryInfo Dir = new DirectoryInfo(dir);
+        FileInfo[] info = Dir.GetFiles("*.png");
+        int TexturesCount = info.Length;
+
+        Debug.Log("TexturesCount" + TexturesCount);
+
+        //use texture count from local path for 'for loop' and add textures to textures list
+        for (int i = 0; i < TexturesCount; i++)
+        {
+            var texture_ = Resources.Load<Texture2D>("" + i);
+            texture.Add(texture_);
+            Debug.Log(texture_.name);
+        }
+    }
+
+    IEnumerator GetTileData()
+    {
+        
+
+        UnityWebRequest TileRequest = UnityWebRequest.Get(TextureURL);
+        yield return TileRequest.SendWebRequest();
+        
+        if(TileRequest.isNetworkError || TileRequest.isHttpError)
+        {
+            Debug.LogError(TileRequest.error);
+            yield break;
+        }
+
+        JSONNode TilesListInfo = JSON.Parse(TileRequest.downloadHandler.text);
+
+        for(int i = 0; i < TilesListInfo.Count; i++)
+        {
+            DATA.Add(TilesListInfo[i]);
+            Debug.Log(TilesListInfo[i]["url"]);
+        }
+
+
+        //UI
+        for(int i = 0; i <DATA.Count; i++)
+        {
+            Debug.Log(DATA[i]["url"]);
+            string TileTextureURL = DATA[i]["url"];
+
+            UnityWebRequest TileTextureRequest = UnityWebRequestTexture.GetTexture(TileTextureURL);
+            yield return TileTextureRequest.SendWebRequest();
+
+            //Debug.Log(TileInfo["url"].Value);
+            if (TileTextureRequest.isNetworkError || TileTextureRequest.isHttpError)
+            {
+                Debug.LogError(TileTextureRequest.error);
+                yield break;
+            }
+
+
+            var isDownloaded = GetBool(i.ToString());
+            Debug.Log(i + "" + isDownloaded);
+            if (!isDownloaded)
+            {
+                byte[] results = TileTextureRequest.downloadHandler.data;
+
+                var dir_ = Path.Combine(dir, i.ToString() + ".png");
+                File.WriteAllBytes(dir_, results);
+                Debug.Log("DIR_" + dir_);
+                
+                texture.Add(DownloadHandlerTexture.GetContent(TileTextureRequest));
+                SetBool(i.ToString(), true);
+            }
+        }
+
+        
+    }
+    public static void SetBool(string key, bool state)
+    {
+        PlayerPrefs.SetInt(key, state ? 1 : 0);
+    }
+
+    public static bool GetBool(string key)
+    {
+        int value = PlayerPrefs.GetInt(key);
+        return value == 1 ? true : false;
+    }
+}
---
+++ VisualEZ-Assessment/Assets/Scripts/UI.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] GameObject WorldEditCanvasPrefab;
+
+    [SerializeField] RawImage RawImage_;
+    [SerializeField] GameObject texturePanel;
+    [SerializeField] GameObject SelectionPanel;
+    TextureSelector textureSelector;
+
+    List<Texture2D> SelectedTextures;
+
+    bool TURN = true; // Turn Raycast On or Off
+
+    void Start()
+    {
+        SelectedTextures = new List<Texture2D>();
+        textureSelector = Camera.main.GetComponent<TextureSelector>();
+
+        //textureSelector.CreateAndApplyMaterial();
+        ListTextures();
+    }
+
+    private void Update()
+    {
+        var camera = Camera.main;
+        var screenRay = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0) && TURN)
+        {
+            if (Physics.Raycast(screenRay, out hit, float.PositiveInfinity))
+            {
+                textureSelector.EditWall = hit.collider.gameObject;
+
+                GameObject EditCanvas = Instantiate(WorldEditCanvasPrefab, textureSelector.EditWall.transform);
+                EditCanvas.GetComponentInChildren<Button>().onClick.AddListener(() => EditWallTexture(textureSelector.EditWall));
+                Debug.Log(hit.collider.name);
+
+                TURN = false;
+            }
+        }
+    }
+
+    void ListTextures()
+    {
+        foreach (Texture2D texture in textureSelector.texture)
+        {
+            RawImage image = Instantiate(RawImage_, texturePanel.transform);
+            image.name = texture.name;
+            image.gameObject.AddComponent<Button>().onClick.AddListener(() => SelectTexture(texture));
+
+            image.texture = texture;
+        }
+    }
+
+    public void SelectTexture(Texture2D texture)
+    {
+        SelectedTextures.Add(texture);
+        
+        for(int i = 0; i < SelectedTextures.Count; i++)
+        {
+            SelectedTextures[i].name = i.ToString();
+        }
+        Debug.Log("Texture_" + texture.name);
+        textureSelector.TempMat.SetTexture("Texture_" + texture.name, texture);
+
+    }
+
+    public void EditWallTexture(GameObject Wall)
+    {
+        TURN = false;
+        textureSelector.CreateMaterial(Wall.name);
+        SelectionPanel.SetActive(true);
+    }
+    public void Done()
+    {
+        SelectedTextures.Clear();
+
+        textureSelector.EditWall.GetComponent<MeshRenderer>().material = textureSelector.TempMat;
+        Destroy(textureSelector.EditWall.GetComponentInChildren<Canvas>().gameObject);
+        SelectionPanel.SetActive(false);
+
+        TURN = true;
+    }
+}
